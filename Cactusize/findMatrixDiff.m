b1976ef9641f2ac8b9b2d7f7278c830c39d4497e
rbm1996 <56166928+rbm1996@users.noreply.github.com>
2023-05-30 17:44:13 +0200

    Delete Cactusize directory

--- Cactusize/findMatrixDiff.m
+++
@@ -1,6 +0,0 @@
-function [rowInd , colInd , weights] = findMatrixDiff(origMat , transMat)
-[rowInd,colInd] = find(origMat ~= transMat);
-linearidx = sub2ind(size(origMat),rowInd,colInd);
-linearMat = origMat(:);
-weights = linearMat(linearidx);
-end
